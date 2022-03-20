48ef469c7a3f1a8714116627ee8c4c377bad61a7
khalid-zein <zeinkhalid004@gmail.com>
2022-03-20 22:39:30 +0300

    customized placing order page

--- main.js
+++ main.js
@@ -13,8 +13,9 @@ var order = document.getElementById("omar");
 
 omar.addEventListener("click", function(event) {
     event.preventDefault();
-    var address = document.getElementById("address").value;
-    var approval = document.getElementById("jamalulleyl");
+    let address = document.getElementById("address").value;
+    let approval = document.getElementById("jamalulleyl");
+
 
     if (address === ""){
         alert("please enter your delivery address!");
@@ -22,7 +23,22 @@ omar.addEventListener("click", function(event) {
             $(".order-pizza").hide();
             $(".ordering").show();
 
-          approval.innerHTML = "Thank you for choosing Husna's pizza,please start your order now for it to be delivered in, " + address + ", in 30mins time";
+          approval.innerHTML = "Thank you for choosing Husna's pizza,please start your order now for it to be delivered in, " + address + ", in 30mins time with 200ksh/= additional for delivery";
    }
 
+
 });
+ let select = document.getElementById("checkout");
+
+ checkout.addEventListener("click", function (event){
+    event.preventDefault();
+    let size = $("#size option:selected").val();
+    let crust = $("#crust option:selected").val();
+    let topping = $("#toppings option:selected").val();
+    let number = $("#number").val();
+    let delivery = 200;
+    let total = (parseInt(size) + parseInt(crust) + parseInt(topping) + delivery) * number;
+    console.log(total);
+
+
+ });
