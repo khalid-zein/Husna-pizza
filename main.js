c557076100fd817f84cc40bf3ca5de1983da1d83
khalid-zein <zeinkhalid004@gmail.com>
2022-03-20 15:57:03 +0300

    customized order page

--- main.js
+++ main.js
@@ -8,3 +8,21 @@ $(document).ready(function(){
         $(".hide").show();
       });
 });
+
+var order = document.getElementById("omar");
+
+omar.addEventListener("click", function(event) {
+    event.preventDefault();
+    var address = document.getElementById("address").value;
+    var approval = document.getElementById("jamalulleyl");
+
+    if (address === ""){
+        alert("please enter your delivery address!");
+    }else if (address){
+            $(".order-pizza").hide();
+            $(".ordering").show();
+
+          approval.innerHTML = "Thank you for choosing Husna's pizza,please start your order now for it to be delivered in, " + address + ", in 30mins time";
+   }
+
+});
